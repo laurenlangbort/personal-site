11f7e5b357cfd11cde399ef841e8c3f4d036b547
lauren-digsfact <lauren@digsfact.com>
2024-09-18 16:15:53 -0400

    finishing touches maybe

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -45,7 +45,7 @@ export default function Project() {
                 />
               </a>
               <p className="pt-3 fw-semibold font-size-20">DigsFact Website</p>
-              <p className="position-relative z-2 fw-medium">
+              <p className="position-relative z-2 fw-medium pb-3">
                 I developed the user interface for DigsFact's website redesign
                 using Angular, HTML, CSS, and JavaScript. Collaborating closely
                 with a UX designer, I translated Figma designs into a fully
@@ -71,7 +71,7 @@ export default function Project() {
               <p className="pt-3 fw-semibold font-size-20">
                 Madrid Metro System
               </p>
-              <p className="position-relative z-2 fw-medium">
+              <p className="position-relative z-2 fw-medium pb-3">
                 I created and managed a SQL database for a metro card service,
                 handling client registration, card recharging, and usage
                 tracking. Implemented triggers to automatically update card
@@ -79,26 +79,44 @@ export default function Project() {
                 management of discounts and client data.
               </p>
             </div>
-            <div className="col mx-auto d-block mt-0 mt-md-3">
+            <div className="col mx-auto d-block mt-0 mt-md-3"> 
+            <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="align-items-center"
+                href="https://github.com/laurenlangbort/fruitytreasures"
+              >
               <img
                 src={ft}
                 alt=""
                 className="shadow-lg circle-img"
                 width="400"
                 height="250"
-              />
-              <p className="pt-3 fw-semibold font-size-20">Fruity Treasures</p>
+              /> 
+              </a>
+              <p className="pt-3 fw-semibold font-size-20">Fruity Treasures</p> 
+              <p className="position-relative z-2 fw-medium pb-3"> 
+              I worked with a team of four to develop an e-commerce site for selling unique fruits from underrepresented cultures. Built with HTML, CSS, and JavaScript, and designed to improve accessibility and usability. The project includes PHP and SQL for cart management and order processing.
+              </p>
             </div>
-            <div className="col mx-auto d-block mt-0 mt-md-3">
-              {" "}
+            <div className="col mx-auto d-block mt-0 mt-md-3 ">
+              {" "} 
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="align-items-center"
+                href="https://github.com/laurenlangbort/UM"
+              >
               <img
                 src={um}
                 alt=""
                 className="shadow-lg circle-img"
                 width="400"
                 height="250"
-              />
-              <p className="pt-3 fw-semibold font-size-20">Universal Machine</p>
+              /> 
+              </a>
+              <p className="pt-3 fw-semibold font-size-20">Universal Machine</p> 
+              <p className="position-relative z-2 fw-medium pb-3"> I worked with a partner to develop a Universal Machine (UM) that simulates a simplified instruction set architecture. Managed memory segmentation using C arrays, implemented core instructions like addition and multiplication, and built an execution cycle to process binary files. Thoroughly tested memory management and instruction execution to ensure correctness and efficiency.</p>
             </div>
           </div>
         </div>
